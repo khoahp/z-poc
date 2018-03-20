b0c2863b3e8ff24387e22f317d1c710095348104
Vu Dang Khoa <khoavd@fds.vn>
2018-03-20 15:24:59 +0700

    Add Authencation module

---
+++ modules/poc-backend-auth/src/main/java/poc/rest/auth/exception/NotFoundException.java
@@ -0,0 +1,24 @@
+package poc.rest.auth.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import aQute.bnd.annotation.ProviderType;
+
+@ProviderType
+public class NotFoundException extends PortalException{
+
+	public NotFoundException() {
+	}
+
+	public NotFoundException(String msg) {
+		super(msg);
+	}
+
+	public NotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public NotFoundException(Throwable cause) {
+		super(cause);
+	}
+}
---
+++ modules/poc-backend-auth/src/main/java/poc/rest/auth/exception/UnauthenticationException.java
@@ -0,0 +1,20 @@
+package poc.rest.auth.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+public class UnauthenticationException extends PortalException {
+	public UnauthenticationException() {
+	}
+
+	public UnauthenticationException(String msg) {
+		super(msg);
+	}
+
+	public UnauthenticationException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+	public UnauthenticationException(Throwable cause) {
+		super(cause);
+	}
+}
